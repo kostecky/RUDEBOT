455d74fbdc2d7a23cf121ef484b763a4f00629d2
Kristian Kostecky <kris@vmfarms.com>
2012-10-27 23:56:18 -0400

    Status output, Wifi reset on server suicide, spacebar is brakes, \ is quit

--- RUDEBOT.ino
+++ RUDEBOT.ino
@@ -18,6 +18,9 @@ const char reversek2 = 's';
 const char leftk2 = 'a';
 const char rightk2 = 'd';
 
+const char stop = ' ';
+const char disconnect = '\\';
+
 boolean alreadyConnected = false;
 byte mac[6];
 WiFiClient organic = NULL;
@@ -29,10 +32,11 @@ int status = WL_IDLE_STATUS;
 int thrust_drivetime = 666;
 int yaw_drivetime = 666;
 int drivetime = 666;
-
+        
 WiFiServer server(8888);
 
 void setup() {
+  // Initialize motors
   md.init();
 
   //Initialize serial and wait for port to open:
@@ -41,7 +45,7 @@ void setup() {
     ; // wait for serial port to connect. Needed for Leonardo only
   }
   
-  if (WiFi.status() == WL_NO_SHIELD) {
+  if ((status = WiFi.status()) == WL_NO_SHIELD) {
     Serial.println("WiFi shield not present"); 
     // don't continue:
     while(true);
@@ -69,6 +73,44 @@ void setup() {
 void loop() {
   // listen for incoming clients
   WiFiClient client = server.available();
+
+  if (status != WiFi.status())
+  {
+    Serial.print("WiFi status changed: ");
+    if ( status == 0 ) {
+      Serial.println("WL_IDLE_STATUS");
+    } else if ( status == 1 ) {
+      Serial.println("WL_NO_SSID_AVAIL");
+    } else if ( status == 2 ) {
+      Serial.println("WL_SCAN_COMPLETED");
+    } else if ( status == 3 ) {
+      Serial.println("WL_CONNECTED");
+    } else if ( status == 4 ) {
+      Serial.println("WL_CONNECT_FAILED");
+    } else if ( status == 5 ) {
+      Serial.println("WL_CONNECTION_LOST");
+    } else if ( status == 6 ) {
+      Serial.println("WL_DISCONNECTED");
+    } else if ( status == 7 ) {
+      Serial.println("WL_NO_SHIELD");
+    }
+      
+    status = WiFi.status();
+  }
+  
+  // Server alive?
+  if (server.status() != 1) {
+    Serial.println("Server died, disconnecting and reconnecting...");
+    if (organic) {
+      organic.flush();
+      organic.stop();
+      organic = NULL;
+    }
+    alreadyConnected = false;
+    WiFi.disconnect();
+    status = WL_IDLE_STATUS;
+    connectWifi();
+  }  
 
   if (client) {
     if (organic && (organic != client)) {
@@ -121,6 +163,9 @@ void loop() {
           drivetime = yaw_drivetime;
           md.setSpeeds(speed,-speed);
           break;
+        case stop:
+          md.setSpeeds(0,0);
+          break;
         case '1':
           speed = 100;
           thrust_drivetime = 600;
@@ -140,6 +185,11 @@ void loop() {
           speed = 200;
           thrust_drivetime = 400;
           yaw_drivetime = 200;
+          break;
+        case disconnect:
+          alreadyConnected = false;
+          client.flush();
+          client.stop();
           break;
         
       } 
