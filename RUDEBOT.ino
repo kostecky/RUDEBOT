5d145ef9398f6fae6c68f627883d474eae461d62
Kristian Kostecky <kris@vmfarms.com>
2012-10-28 23:24:39 -0400

    Major refactor. Added SD card logging =). Consumed all memory, so I spent some time removing all unnecessary strings and cut down on code.

--- RUDEBOT.ino
+++ RUDEBOT.ino
@@ -1,12 +1,25 @@
 #include <SPI.h>
 #include <WiFi.h>
+#include <SD.h>
 #include "DualMC33926MotorShield.h"
 #include "keys.h"
 
 // Every 60 seconds shoot across a keepalive
 #define KEEPALIVE 60000
 
+// Every second we run something
+#define CRON 1000
+
+// Buffer for our logging function
+#define MAXLOG 64
+
+// Restart server interval?
+#define SERVER 30000
+
 DualMC33926MotorShield md;
+
+// Where's the SD card?
+const int sdSelect = 4;
 
 const char forwardk = 'k';
 const char reversek = 'j';
@@ -21,11 +34,21 @@ const char rightk2 = 'd';
 const char stop = ' ';
 const char disconnect = '\\';
 
+// String table to save memory
+const char* oneConn = "Sorry, one at a time\r\n";
+const char* hello = "Hello!\r\n";
+const char* signalString = "Signal: %ld dBm\r\n";
+const char* serverString = "Server status: %d\r\n";
+const char* clientString = "Client status: %d\r\n";
+const char* bye = "Bye!\r\n";
+
 boolean alreadyConnected = false;
 byte mac[6];
 WiFiClient organic = NULL;
 unsigned long keepalive = millis();
 unsigned long lastc = millis();
+unsigned long lastcron = millis();
+unsigned long lastserver = millis();
 int speed = 100;
 int status = WL_IDLE_STATUS;
 // How many milliseconds do we drive for in between commands?
@@ -34,24 +57,53 @@ int yaw_drivetime = 666;
 int drivetime = 666;
         
 WiFiServer server(8888);
+WiFiClient client = NULL;
+File logFile;
+
+void logger(const char *fmt, ...) {
+  char buffer[MAXLOG];
+  memset(buffer, '\0', MAXLOG);
+  va_list args;
+  va_start (args, fmt );
+  vsnprintf(buffer, MAXLOG, fmt, args);
+  va_end (args);
+
+  Serial.print(buffer);
+
+  if (logFile) {
+    logFile.print(buffer);
+    logFile.flush();
+  } else {
+    Serial.println("Log error!");
+  }   
+}
 
 void setup() {
-  // Initialize motors
-  md.init();
-
   //Initialize serial and wait for port to open:
   Serial.begin(9600); 
   while (!Serial) {
     ; // wait for serial port to connect. Needed for Leonardo only
   }
+
+  // Initialize motors
+  md.init();
+
+  // Initialize SD card
+  if (!SD.begin(sdSelect)) {
+    Serial.print("Card failed or not present\r\n");
+  } else {
+    Serial.print("SD initialized\r\n");
+  }
+  logFile = SD.open("RUDEBOT.log", FILE_WRITE);
   
   if ((status = WiFi.status()) == WL_NO_SHIELD) {
-    Serial.println("WiFi shield not present"); 
+    logger("WiFi shield not present\r\n"); 
     // don't continue:
     while(true);
   } 
 
   // Spit out MAC 
+  /*
   WiFi.macAddress(mac);
   Serial.print("MAC: ");
   Serial.print(mac[5],HEX);
@@ -65,6 +117,7 @@ void setup() {
   Serial.print(mac[1],HEX);
   Serial.print(":");
   Serial.println(mac[0],HEX);
+  */
   
   // Connect
   connectWifi();
@@ -72,71 +125,54 @@ void setup() {
 
 void loop() {
   // listen for incoming clients
-  WiFiClient client = server.available();
+  client = server.available();
 
-  if (status != WiFi.status())
-  {
-    Serial.print("WiFi status changed: ");
-    if ( status == 0 ) {
-      Serial.println("WL_IDLE_STATUS");
-    } else if ( status == 1 ) {
-      Serial.println("WL_NO_SSID_AVAIL");
-    } else if ( status == 2 ) {
-      Serial.println("WL_SCAN_COMPLETED");
-    } else if ( status == 3 ) {
-      Serial.println("WL_CONNECTED");
-    } else if ( status == 4 ) {
-      Serial.println("WL_CONNECT_FAILED");
-    } else if ( status == 5 ) {
-      Serial.println("WL_CONNECTION_LOST");
-    } else if ( status == 6 ) {
-      Serial.println("WL_DISCONNECTED");
-    } else if ( status == 7 ) {
-      Serial.println("WL_NO_SHIELD");
-    }
-      
-    status = WiFi.status();
-  }
-  
-  // Server alive?
-  if (server.status() != 1) {
-    Serial.println("Server died, disconnecting and reconnecting...");
-    if (organic) {
-      organic.flush();
-      organic.stop();
-      organic = NULL;
-    }
+  // Reset already connected if someone dropped
+  if (((alreadyConnected == true) && !client) || 
+      ((alreadyConnected == true) && client && client.status() != 4)) {
     alreadyConnected = false;
-    WiFi.disconnect();
-    status = WL_IDLE_STATUS;
-    connectWifi();
-  }  
+  }
+    
+  // Jobs to run every second
+  if ( (millis() - lastcron) > CRON) {
+    logger(signalString, WiFi.RSSI());
+    logger(serverString, server.status());
+    if (client) {
+      logger(clientString, client.status());
+    }
+    lastcron = millis();
+    
+    // Server alive?
+    if (server.status() != 1) {
+      logger("Server dead: %d\r\n", server.status());      
+      if (client) {
+        killClient();
+        return;
+      }
+      md.setSpeeds(0,0);
+    } else {
+      // Log it if it's been a while since we were alive
+      if ( (millis()-lastserver) > 5000 ) {
+        logger("Server dead for %d seconds\r\n", (millis()-lastserver)/1000);
+      }
+      lastserver = millis();
+    }
+  }
+
+  int newstatus = WiFi.status();
+  if (status != newstatus)
+  {
+    logger("WiFi status changed to: %d\r\n", newstatus);
+    status = newstatus;
+  }
 
   if (client) {
-    if (organic && (organic != client)) {
-      Serial.println("Sorry, only one organic lifeform at a time");
-      server.write("Sorry, only one organic lifeform at a time\n");
-      alreadyConnected = false;
-      client.flush();
-      client.stop();
-    }    
-    
     // New clients
     if (!alreadyConnected) {
-      Serial.println("New lifeform detected");
-      server.write("New lifeform detected\n");
+      logger(hello);
+      server.write(hello);
       alreadyConnected = true;
       client.flush();
-      organic = client;
-    }
-
-    // Kill old clients 
-    if (organic && !organic.connected()) {
-      Serial.println("Severing mind link");
-      organic.flush();
-      organic.stop();
-      organic = NULL;
-      alreadyConnected = false;
     }
 
     if (client.available()) {
@@ -146,12 +182,12 @@ void loop() {
         case forwardk:
         case forwardk2:
           drivetime = thrust_drivetime;
-          md.setSpeeds(speed,speed);          
+          md.setSpeeds(-speed,-speed);          
           break;
         case reversek:
         case reversek2:
           drivetime = thrust_drivetime;
-          md.setSpeeds(-speed,-speed);
+          md.setSpeeds(speed,speed);
           break;
         case leftk:
         case leftk2:
@@ -187,9 +223,7 @@ void loop() {
           yaw_drivetime = 200;
           break;
         case disconnect:
-          alreadyConnected = false;
-          client.flush();
-          client.stop();
+          killClient();
           break;
         
       } 
@@ -206,23 +240,30 @@ void loop() {
       }
       // keepalive (annoying!!)
       if ( (millis() - keepalive) > KEEPALIVE) {
-        if (organic)
-          organic.flush();
-        server.write(".");
-        Serial.println("Ping!");
+        if (client)
+          client.flush();
+        server.write(".\r\n");
+        Serial.print("Ping!\r\n");
         keepalive = millis();
       }
     }
   } 
 }
 
+void killClient() {
+  logger(bye);
+  server.write(bye);
+  client.flush();
+  client.stop();
+  alreadyConnected == false;
+}
+  
 void connectWifi() {
   while (status != WL_CONNECTED) { 
-    Serial.print("Attempting to connect to SSID: ");
-    Serial.println(ssid);
+    logger("Attempting to connect to SSID: %s\r\n", ssid);
     status = WiFi.begin(ssid, pass);
     if (status != WL_CONNECTED) { 
-      Serial.println("Couldn't get a wifi connection, retrying...");
+      logger("Couldn't connect via WiFi, retrying\r\n");
     } else {
       server.begin();
       printWifiStatus();
@@ -232,29 +273,25 @@ void connectWifi() {
 
 void printWifiStatus() {
   // Firmware version?
-  Serial.println("Firmware Version: " + String(WiFi.firmwareVersion()));
+  // Serial.println("Firmware Version: " + String(WiFi.firmwareVersion()));
   
   // print the SSID of the network you're attached to:
-  Serial.print("SSID: ");
-  Serial.println(WiFi.SSID());
-
+  logger("SSID: %s\r\n", WiFi.SSID());
+  
   // print your WiFi shield's IP address:
   IPAddress ip = WiFi.localIP();
-  Serial.print("IP Address: ");
-  Serial.println(ip);
+  logger("IP Address: %d.%d.%d.%d\r\n", ip[0], ip[1], ip[2], ip[3]);
 
   // print the received signal strength:
-  long rssi = WiFi.RSSI();
-  Serial.print("signal strength (RSSI):");
-  Serial.print(rssi);
-  Serial.println(" dBm");
+  logger(signalString, WiFi.RSSI());
+  logger(serverString, server.status());
 }
 
 void stopIfFault()
 {
   if (md.getFault())
   {
-    Serial.println("fault");
+    logger("Motor fault\r\n");
     while(1);
   }
 }
